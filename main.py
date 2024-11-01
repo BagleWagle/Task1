c1e5e5b67d46b1019f42a9bf0f815ce04abeba51
BagleWagle <bzijer@gmail.com>
2024-11-01 19:29:08 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#print("Hello World")
+#print(type(5>10)) # int - integer целое, float дробное, str - string строка, bool - boolean логический
+# // - целочисленное, / - деление, ** - степень
+#print('Hello ' + 'World')        #concatenate
+#print(5 < 10)
+
+x=10
+if x == 10:
+    print('yeaaahh')
+else:
+    print('oh nooo')
+print(x+5)
+
+
